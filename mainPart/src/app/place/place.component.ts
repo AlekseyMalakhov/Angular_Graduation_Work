0d5aa1b79e1413270bd50d24e2597ec0031d1258
Aleksey Malakhov <hexel@tut.by>
2020-04-02 10:38:33 +0300

    upd

--- mainPart/src/app/place/place.component.ts
+++ mainPart/src/app/place/place.component.ts
@@ -11,7 +11,6 @@ export class PlaceComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.place.name);
   }
 
 }
--- mainPart/src/app/point/point.component.ts
+++ mainPart/src/app/point/point.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-point',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./point.component.css']
 })
 export class PointComponent implements OnInit {
+  @Input() place;
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit(): void {
+  }
+
+  getCoords() {
+    return {
+      left: this.place.coords[0] + "px", 
+      top: this.place.coords[1] + "px"
+    };
   }
 
 }

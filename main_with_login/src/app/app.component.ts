65ca821c038954732ec9ff31c02a5b4ea626a61a
Aleksey Malakhov <hexel@tut.by>
2020-04-08 18:26:07 +0300

    upd

--- main_with_login/src/app/app.component.ts
+++ main_with_login/src/app/app.component.ts
@@ -1,7 +1,65 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { OktaAuthService } from '@okta/okta-angular';
-import places from "./places.json";
+//import places from "./places.json";
+
+const places = [
+  {
+    id: 1,
+    name: 'Lorem',
+    author: 'Stipe Miocic',
+    img: '1.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius ornare est in porttitor. Nunc congue feugiat risus ut auctor. Maecenas ',
+    coords: [
+      700,
+      500
+    ]
+  },
+  {
+    id: 2,
+    name: 'Ipsum',
+    author: 'Khabib Nurmagamedov',
+    img: '2.jpg',
+    description: 'imperdiet velit malesuada ex lobortis interdum. Fusce odio lectus, varius eget est non, porta aliquam sapien. Ut porta, eros in porttitor ',
+    coords: [
+      1000,
+      700
+    ]
+  },
+  {
+    id: 3,
+    name: 'Dolor',
+    author: 'Conor McGregor',
+    img: '3.jpg',
+    description: 'aliquam, lectus quam pharetra elit, nec facilisis tellus magna et ante. Curabitur ornare ac sem nec suscipit. Pellentesque est purus, ',
+    coords: [
+      1650,
+      600
+    ]
+  },
+  {
+    id: 4,
+    name: 'Vivamus',
+    author: 'Mirko Cro Cop',
+    img: '4.jpg',
+    description: 'elementum a vehicula vel, tempor ac sapien. Integer volutpat eu libero vel congue. Suspendisse semper malesuada rutrum. Aliquam',
+    coords: [
+      300,
+      200
+    ]
+  },
+  {
+    id: 5,
+    name: 'Aliquam',
+    author: 'Fedor Emelianenko',
+    img: '5.jpg',
+    description: 'tincidunt rutrum lobortis. Proin maximus diam eget quam aliquet, et ullamcorper arcu pharetra. Ut malesuada justo pellentesque nulla ',
+    coords: [
+      1650,
+      300
+    ]
+  }
+]
 
 @Component({
   selector: 'app-root',

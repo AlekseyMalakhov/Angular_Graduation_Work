1a1c013bf83b4260e389e366d62ddf4d0fd7f162
Aleksey Malakhov <hexel@tut.by>
2020-04-27 10:56:30 +0300

    upd

--- main_with_login/src/polyfills.ts
+++
@@ -1,7 +0,0 @@
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
-import { TextEncoder } from 'text-encoding';
-if (typeof (window as any).TextEncoder === 'undefined') {
-  (window as any).TextEncoder = TextEncoder;
-}

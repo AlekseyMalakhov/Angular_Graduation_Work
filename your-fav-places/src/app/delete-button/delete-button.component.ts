f28cb20960d14739ad9301b88e4a10f96a8a0f30
Aleksey Malakhov <hexel@tut.by>
2020-04-29 13:18:18 +0300

    upd

--- your-fav-places/src/app/delete-button/delete-button.component.ts
+++ your-fav-places/src/app/delete-button/delete-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PlacesService } from "../places.service";
 
 @Component({
   selector: 'app-delete-button',
@@ -6,14 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-button.component.css']
 })
 export class DeleteButtonComponent implements OnInit {
+  @Input() del_place_id;
+  places;
 
-  constructor() { }
+  constructor(private data: PlacesService) {
+    this.data.currentPlaces.subscribe(places => this.places = places);
+  }
 
   ngOnInit(): void {
   }
 
-  deletePlace() {
-    console.log("delete place");
+  deletePlace() {    
+    var new_list = [...this.places];
+    var pos = new_list.findIndex((e) => (e.id === this.del_place_id));
+    console.log("pos = " + pos);
+    console.log("del_place_id = " + this.del_place_id);
+    if (pos !== -1) {
+      new_list.splice(pos, 1);
+      this.data.changePlaces(new_list);
+      console.log(new_list); 
+    } else {
+      console.log("no such position");
+    }
   }
 
 

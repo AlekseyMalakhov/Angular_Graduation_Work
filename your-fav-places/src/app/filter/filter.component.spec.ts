ad097804fb5c9efab245a3529579fe45f7677803
Aleksey Malakhov <hexel@tut.by>
2020-05-06 17:01:45 +0300

    upd

--- your-fav-places/src/app/filter/filter.component.spec.ts
+++ your-fav-places/src/app/filter/filter.component.spec.ts
@@ -1,14 +1,72 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { PlacesService } from "../places.service";
 
 import { FilterComponent } from './filter.component';
 
-xdescribe('FilterComponent', () => {
+var test_places = [
+  {
+    id: 1,
+    name: "Minsk",
+    author: "Aleksey Malakhov",
+    author_id: "aleksey_malakhov",
+    img: '1.jpg',
+    description: "Very cosy place",
+    coords: [700, 300],
+  },
+  {
+    id: 2,
+    name: "Grodno",
+    author: "Sergey Sergeev",
+    author_id: "sergey_sergeev",
+    img: '3.jpg',
+    description: "Cool place to have some rest",
+    coords: [500, 1200],
+  },
+];
+
+var filter = {
+  author: "Sergey Sergeev",
+  min_lat: 0,
+  max_lat: 0,
+  min_long: 0,
+  max_long: 0,
+};
+
+//var filtered_places = [];
+
+class MockService {  
+  private placesList = new BehaviorSubject(test_places);       //список мест
+  currentPlaces = this.placesList.asObservable();
+  changePlaces(newPlaces) {
+    this.placesList.next(newPlaces);
+  }
+
+  private userPage = new BehaviorSubject(false);             //проверяем если мы на странице пользователя (тогда показываем только его места)
+  currentUserPage = this.userPage.asObservable();
+  changeUserPage(user_page: boolean) {
+    this.userPage.next(user_page);
+  }
+
+  private filterDisplay = new BehaviorSubject(true);          //переключаем отображение фильтра
+  currentFilterDisplay = this.filterDisplay.asObservable();
+  changeFilterDisplay(filter_display: boolean) {
+    this.filterDisplay.next(filter_display);
+  }
+
+  changeFilteredPlaces(arr) {}
+}
+
+describe('FilterComponent', () => {
   let component: FilterComponent;
   let fixture: ComponentFixture<FilterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FilterComponent ]
+      declarations: [ FilterComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [{provide: PlacesService, useClass: MockService}],
     })
     .compileComponents();
   }));
@@ -22,4 +80,13 @@ xdescribe('FilterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Должен фильтровать', () => {
+    component.filter = filter;
+    component.ngDoCheck();
+    //console.log(component.filtered_places);
+    expect(component.filtered_places[0]).toEqual(undefined);
+    expect(component.filtered_places[1].name).toEqual("Grodno");
+  });
+  
 });
--- your-fav-places/src/app/user-places-page/user-places-page.component.spec.ts
+++ your-fav-places/src/app/user-places-page/user-places-page.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 
 import { UserPlacesPageComponent } from './user-places-page.component';
 
@@ -14,6 +16,7 @@ describe('UserPlacesPageComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ UserPlacesPageComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
       providers: [{provide: ActivatedRoute, useClass: ActivatedRouteStub}],
     })
     .compileComponents();

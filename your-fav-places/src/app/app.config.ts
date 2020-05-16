efc444e9fdbc87fd00185b343caea885d2c3abff
Aleksey Malakhov <hexel@tut.by>
2020-05-16 12:57:44 +0300

    Update app.config.ts

--- your-fav-places/src/app/app.config.ts
+++ your-fav-places/src/app/app.config.ts
@@ -3,7 +3,7 @@ export default {
     clientId: "0oa48iu3oCyIWyO3I4x6",
     issuer: "https://dev-843586.okta.com/oauth2/default",
     /*redirectUri: 'http://localhost:4200/implicit/callback',*/
-    redirectUri: 'https://your-favorite-places.ew.r.appspot.com:8081/implicit/callback',
+    redirectUri: 'https://your-favorite-places.ew.r.appspot.com/implicit/callback',
     scopes: ['openid', 'profile', 'email'],
   },
   resourceServer: {

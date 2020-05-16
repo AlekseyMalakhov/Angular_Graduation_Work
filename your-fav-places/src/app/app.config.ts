32126ea695172d720c050a595f76f40f82a9f9b5
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2020-05-17 00:57:32 +0300

    upd

--- your-fav-places/src/app/app.config.ts
+++ your-fav-places/src/app/app.config.ts
@@ -2,11 +2,13 @@ export default {
   oidc: {
     clientId: "0oa48iu3oCyIWyO3I4x6",
     issuer: "https://dev-843586.okta.com/oauth2/default",
-    /*redirectUri: 'http://localhost:4200/implicit/callback',*/
+    //redirectUri: 'http://localhost:4200/implicit/callback',
     redirectUri: 'https://your-favorite-places.ew.r.appspot.com/implicit/callback',
     scopes: ['openid', 'profile', 'email'],
   },
+  /*
   resourceServer: {
     messagesUrl: 'http://localhost:8000/api/messages',
   },
+  */
 };
